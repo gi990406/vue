adf65d22c34aa5d6944a9d47a3b3b8cecd0af722
gi990406 <rlshdnr4550@nate.com>
2023-08-24 15:10:36 +0900

    new upload

---
+++ 반응속도체크/ResponseCheck.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <div id="screen" :class="state" @click="onClickScreen">{{message}}</div>
+        <div>
+            <div>평균 시간: {{}}</div>
+            <button @click="onReset">리셋</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                result: [],
+                state: 'waiting',
+                message: '클릭해서 시작',
+            }
+        },
+        methods: {
+            onReset() {
+
+            },
+            onClickScreen() {
+                if (this.state ==='waiting') {
+                    this.state = 'ready';
+                    this.message = '초록색이 되면 클릭';
+                    setTimeout(() => {
+                        this.state='now';
+                        this.message = '지금 클릭';
+                }, Math.floor(Math.random() * 1000) + 2000);
+                } else if (this.state === 'ready') {
+                    clearTimeout(timeout);
+                    this.state = 'now';
+                    this.message = '너무 성급';
+                } else if (this.state === 'now') {
+                    this.state = 'waiting';      
+                    this.message = '클릭해서 시작하세요';
+                    this.result.push(endTime - startTime);
+                }
+            }
+        },
+    };
+</script>
+
+<script>
+</script>
+
+<style scoped>
+    #screen {
+        width: 300px;
+        height: 300px;
+        text-align: center;
+        user-select: none;
+    }
+    #screen.waiting {
+        background-color: aqua;
+    }
+    #screen.ready {
+        background-color: red;
+        color: white;
+    }
+    #screen.now {
+        background-color: greenyellow;
+    }
+</style>
